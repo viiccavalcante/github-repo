58afe0a17b0205209c162ef5e9a4e65fbd0792a6
victoria <victoriapcavalcante@gmail.com>
2025-03-04 20:02:36 +0100

    deleting unused files and styling the home page

--- src/main.tsx
+++ src/main.tsx
@@ -3,21 +3,17 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.css';
 import { Home } from './pages/Home.tsx';
-import Navbar from './components/shared/Navbar.tsx';
 import { UserRepos } from './pages/UserRepos.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Router>
-      {/* <div className="app-container">
-        <Navbar /> */}
-        <main className="container mx-auto mt-4">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/profile/:username" element={<UserRepos />} />
-          </Routes>
-        </main>
-      {/* </div> */}
+      <main className="container mx-auto mt-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile/:username" element={<UserRepos />} />
+        </Routes>
+      </main>
     </Router>
   </StrictMode>
 )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,21 @@
-import { useForm } from 'react-hook-form'
-import { useNavigate } from 'react-router-dom';
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { styled } from "styled-components";
+import {  
+  ColumnContainer,
+  Footer, 
+  Input,
+  Title, 
+  MdButton, 
+  RotatedArrow } from "../components/shared/GlobalStyle";
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 95vh;
+  width: 100%;
+`;
 
 export function Home() {
   const navigate = useNavigate();
@@ -7,20 +23,35 @@ export function Home() {
 
   const onSubmit = (data: { searchText: string }) => {
     const username = data.searchText.trim();
-    if (data.searchText.trim()) {
+    if (username) {
       navigate(`/profile/${username}`);
     }
-  }
+  };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <input {...register('searchText')} placeholder="Github username" />
-        </div>
-        <button type="submit">Find User</button>
-        <a href='#'> About me </a>
-      </form>
-    </div>
-  )
+    <Wrapper>
+      <ColumnContainer>
+
+        <Title>
+          Search for Github repositories:
+        </Title>
+  
+        <form onSubmit={handleSubmit(onSubmit)} style={{ width: "90%"}} >
+          <Input {...register("searchText")} placeholder="Username" />
+
+          <MdButton type="submit" style={{marginTop: "10px"}}>
+            Search <RotatedArrow size={20} />
+          </MdButton>
+        </form>
+  
+        <Footer>
+          <a href="#">
+            About Me
+          </a>
+        </Footer>
+
+      </ColumnContainer>
+    </Wrapper>  
+  );
 }
+
